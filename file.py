b57b71399c4faf47c90c4088c959025dc0137a94
Ali-Nawab <alinawabkhan15@gmail.com>
2022-07-21 16:33:21 +0500

    add

--- file.py
+++ file.py
@@ -1,4 +1,8 @@
-pirnt("what are you doing)
-      a = 10
-      b = a * 10
-      print(b)
+print("what are you doing")
+a = 10
+b = a * 10
+print(b)
+
+print("we belong to pakistan")
+c = a + b
+print(c)
